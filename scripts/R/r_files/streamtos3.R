d76a386e165506c8364d67b635ad32abafd58c81
Freddy Drennan <drennanfreddy@gmail.com>
2020-04-23 14:27:22 -0600

    update

--- scripts/R/r_files/streamtos3.R
+++ scripts/R/r_files/streamtos3.R
@@ -10,7 +10,7 @@ tryCatch({
   now_time_zip <- glue('stream_{nowtime}.zip')
   fs::file_move(path = 'stream.csv', new_path = now_time_csv)
   data <- read_csv('now_time_csv')
-  dbAppendTable(conn = con, name = 'streamall', data)
+  dbAppendTable(conn = con, name = 'streamall', value = data)
   zip(zipfile = now_time_zip, files = now_time_csv)
   s3_upload_file(bucket = 'reddit-dumps', from = now_time_zip, to = now_time_zip, make_public = TRUE)
   file_size <- as.character(fs::file_info(now_time_csv)$size)
