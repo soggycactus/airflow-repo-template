469831b9bc5c25051120313c1405462bf5937b61
Freddy Drennan <drennanfreddy@gmail.com>
2020-04-23 14:34:22 -0600

    update

--- scripts/R/r_files/streamtos3.R
+++ scripts/R/r_files/streamtos3.R
@@ -9,7 +9,13 @@ tryCatch({
   now_time_csv <- glue('stream_{nowtime}.csv')
   now_time_zip <- glue('stream_{nowtime}.zip')
   fs::file_move(path = 'stream.csv', new_path = now_time_csv)
-  data <- read_csv('now_time_csv')
+  data <- read_csv('now_time_csv', col_names = FALSE)
+  colnames(data) <- c("author", "author_fullname", "author_patreon_flair", "author_premium", 
+                      "body", "can_gild", "can_mod_post", "controversiality", "created", 
+                      "created_utc", "depth", "downs", "fullname", "id", "is_root", 
+                      "is_submitter", "link_id", "name", "no_follow", "parent_id", 
+                      "permalink", "score", "submission", "subreddit", "subreddit_id", 
+                      "total_awards_received", "ups", "time_gathered_utc")
   dbAppendTable(conn = con, name = 'streamall', value = data)
   zip(zipfile = now_time_zip, files = now_time_csv)
   s3_upload_file(bucket = 'reddit-dumps', from = now_time_zip, to = now_time_zip, make_public = TRUE)
