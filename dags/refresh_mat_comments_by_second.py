86b450416736cf469b5b4474fe63d433d3a02595
Freddy Drennan <drennanfreddy@gmail.com>
2020-04-25 09:29:22 -0600

    update

---
+++ dags/refresh_mat_comments_by_second.py
@@ -0,0 +1,25 @@
+import airflow
+from airflow.operators.bash_operator import BashOperator
+from airflow.models import DAG
+
+args = {
+    'owner': 'Freddy Drennan',
+    'start_date': airflow.utils.dates.days_ago(2),
+    'email': ['drennanfreddy@gmail.com'],
+    'retries': 100,
+    'email_on_failure': True,
+    'email_on_retry': True
+}
+dag = DAG(dag_id='refresh_mat_comments_by_second',
+          default_args=args,
+          schedule_interval='@hourly',
+          concurrency=1,
+          max_active_runs=1,
+          catchup=False)
+
+task_1 = BashOperator(
+    task_id='streamtos3',
+    bash_command='. /home/scripts/R/shell/refresh_mat_comments_by_second',
+    dag=dag
+)
+
