01ecd92748e033d911434aa4d90cf51402ac843c
LCStephens <lcstephens@utexas.edu>
2020-10-30 20:45:09 -0500

    new version of black: autoformatting

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 setup(
     name="plugins",
     entry_points={
-        "airflow.plugins": ["example_plugin = plugins.example_plugin:ExamplePlugin",]
+        "airflow.plugins": [
+            "example_plugin = plugins.example_plugin:ExamplePlugin",
+        ]
     },
 )
